9fe97ad728376fd86689d9ddc41421b4cb7941db
Robert Farb <farb.robert@gmail.com>
2018-08-30 16:46:05 -0700

    Add heroku db creds

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://nadav:nadav123456@ds015334.mlab.com:15334/flex_project',
+  prodMongoURI: 'mongodb://heroku_bqnhj9mh:emmng6buljgtii21vddqej6o5i@ds137862.mlab.com:37862/heroku_bqnhj9mh',
+  secretOrKey: 'secret'
+};
